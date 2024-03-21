b83c324e9eb52b46fc77ee5ff86f6920d06f79a4
ameerhamzashah <160713923+ameerhamzashah1@users.noreply.github.com>
2024-03-21 09:58:19 +0500

    these are my repositories

---
+++ relation.js
@@ -0,0 +1,19 @@
+//comparison operator 
+// it prints in boolean type form: true/false
+var num_1 = 15;
+var num_2 = 15;
+var num_3 = 30;
+var num_4 = 25;
+var num_5 = 20;
+console.log(num_1 === num_2);
+console.log(num_1 != num_3);
+console.log(num_2 === num_3);
+console.log(num_2 != num_4);
+console.log(5 * 5 == num_4);
+console.log(10 == 10);
+console.log(num_1 < num_2);
+console.log(num_1 < num_3);
+console.log(num_4 < num_2);
+console.log(num_2 < num_1);
+console.log(num_2 <= num_3);
+console.log(num_2 < num_1);
---
+++ relation.ts
@@ -0,0 +1,25 @@
+
+
+
+
+//comparison operator is also called relation operator:
+
+// it prints in boolean type form: true/false
+let num_1 = 15;
+let num_2 = 15;
+let num_3 = 30;
+let num_4 = 25;
+let num_5 = 20;
+
+console.log(num_1 === num_2);        // is equal to
+console.log(num_1 != num_3);     // is not equal to 
+console.log(num_2 === num_3);       // is equals to
+console.log(num_2 != num_4);     //is not equals to 
+console.log(5*5==num_4) ;           // is equal to
+console.log(10==10);             // is equal to
+console.log(num_1<num_2);               // lessthen
+console.log(num_1<=num_3);               // is equal to or lessthen
+console.log(num_4>num_2);       //greaterthen
+console.log(num_2<num_1);         //lessthen
+console.log(num_2<=num_3);             // lessthen is equal to
+console.log(num_2!<num_1);     //not lessthen 
